170720c48a1ba94f589f7fd03f6399c7c8e9198a
GDBlaster <65135527+GDBlaster@users.noreply.github.com>
2025-12-03 00:03:30 +0100

    lualine

--- config/modules/lualine.nix
+++ config/modules/lualine.nix
@@ -1,5 +1,11 @@
 {
   plugins.lualine = {
     enable = true;
-  };
-}
+    settings = {
+      options = {
+        ignore_focus = [ "NvimTree" ];
+      };
+      extensions = ["nvim-tree"];
+    };
+  };        
+} 
